9d348f692ad49ba66e195249b6de26c4ae24ccf4
neil patel <neil@myindianweddingapp.com>
2015-10-26 14:58:38 +0000

    Updated region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
    # config/environments/production.rb
 config.paperclip_defaults = {
   :storage => :s3,
-    region: ENV['AWS_REGION'],
+    :region ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
