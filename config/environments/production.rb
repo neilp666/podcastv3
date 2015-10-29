4a5224143507dc1c7a09ce73e937cb67aef6f373
neil patel <neil@myindianweddingapp.com>
2015-10-29 15:07:19 +0000

    Updated environments production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ config.paperclip_defaults = {
      :bucket => ENV['S3_BUCKET_NAME'],
      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-     :s3_host_name => 's3-us-west-2.amazonaws.com',
+     :s3_host_name => 'us-west-2',
   }
 }
 end
